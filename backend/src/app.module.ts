e10441ff2346cbd5055d7b5d19dbb99c4e008850
Sajjad Matin <sajjadmatinm@gmail.com>
2026-02-26 08:32:09 +0430

    feat: Created Property Route: -Module -Controller -Service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,8 +4,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AgenciesModule } from './agencies/agencies.module';
 import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
+import { PropertiesModule } from './properties/properties.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, AgenciesModule, CommonModule, UsersModule],
+  imports: [AuthModule, PrismaModule, AgenciesModule, CommonModule, UsersModule, PropertiesModule],
 })
 export class AppModule {}
