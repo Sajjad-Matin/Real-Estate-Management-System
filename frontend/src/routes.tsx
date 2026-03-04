3b5cb8a221fffe135479ca6e7a72ee2e77b2229d
Shafiullah Sabit <shafiuallahsabit@gmail.com>
2026-03-04 19:18:59 +0430

    test

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>about</div>,
+    element: <div>aabout</div>,
   },
 ]);
 
