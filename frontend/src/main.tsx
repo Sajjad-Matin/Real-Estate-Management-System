433353f59fdb964d73c2b1d73b41678707b2f6ee
Shafiullah Sabit <shafiuallahsabit@gmail.com>
2026-03-04 19:15:09 +0430

    about page added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./routes";
-
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
@@ -14,3 +13,6 @@ createRoot(document.getElementById("root")!).render(
     </QueryClientProvider>
   </StrictMode>,
 );
+
+
+
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home</div>,
+    element: <div>about</div>,
   },
 ]);
 
