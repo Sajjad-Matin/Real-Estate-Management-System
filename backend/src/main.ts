77413b11e79d5c1c1b0685a28bf18647c30b7879
Sajjad-Matin <sajjadmatinm@gmail.com>
2026-08-08 11:34:59 +0430

    Refactor CORS settings in main.ts
    Updated CORS configuration to include OPTIONS method and use arrays for methods and allowedHeaders.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,9 +15,12 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('api');
   app.enableCors({
-  origin: ['http://localhost:5173', 'https://real-estate-management-system-ivory.vercel.app'],
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
+  origin: [
+    'http://localhost:5173',
+    'https://real-estate-management-system-ivory.vercel.app',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 });
   await app.listen(process.env.PORT ?? 3000);
